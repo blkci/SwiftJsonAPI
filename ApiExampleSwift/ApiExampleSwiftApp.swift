5a755a7926a1c5c66764eb75a43f89365e3c03be
blkci <muhammetbalikci0@outlook.com>
2023-07-11 20:39:45 +0300

    Initial Commit

---
+++ ApiExampleSwift/ApiExampleSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiExampleSwiftApp.swift
+//  ApiExampleSwift
+//
+//  Created by Muhammet Balıkçı on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ApiExampleSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
